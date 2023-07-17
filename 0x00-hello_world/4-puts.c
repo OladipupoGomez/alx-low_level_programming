72f4eac253c94acb24ff0f4558b2931743b3bd6e
Oladipupo Gomez <Oladipupogomez5@gmail.com>
2023-07-16 18:10:17 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,11 @@
-#!/bin/bash
-puts(Programming is like building a multilingual puzzl)/n
+#include <stdio.h>
+/**
+*main -entry point
+*return -entry point
+*retun: always 0 (sucess)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+}
