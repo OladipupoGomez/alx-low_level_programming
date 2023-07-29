26b15613a5a564ef6f36fe37560feac455dfbf62
Oladipupo Gomez <Oladipupogomez5@gmail.com>
2023-07-28 17:45:35 -0700

    magic file

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+/*main - update magic.c*/
+
 int main(void)
 {
 int n;
@@ -15,9 +17,8 @@ p = &n;
 * - you are not allowed to modify p
 * - only one statement
 * - you are not allowed to code anything else than this line of code
-*main - update magic.c
 */
-*(p+5) = 98;
+*(p + 5) = 98;
 /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
 return (0);
