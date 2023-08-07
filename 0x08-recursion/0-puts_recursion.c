062a9143758306fd58ecc7b6552d8ccc344f1637
Oladipupo Gomez <Oladipupogomez5@gmail.com>
2023-08-07 00:27:46 -0700

    function that prints a string -recursion task 1

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+*_puts_recursion- function that prints a string followed by a new line
+*@s: string to be printed
+*
+*Return: 0
+*/
+
+void _puts_recursion(char *s);
+char s = string
+puts("s\n")
