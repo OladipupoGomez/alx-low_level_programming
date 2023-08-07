91901d2bd832f05d3347090dd78159af10c66f8b
Oladipupo Gomez <Oladipupogomez5@gmail.com>
2023-08-07 04:36:07 -0700

    function that prints a string on a new line - recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,6 +7,14 @@
 *Return: 0
 */
 
-void _puts_recursion(char *s);
-char s = string
-puts("s\n")
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+
+	else
+		_putchar('\n');
+}
