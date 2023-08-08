a81b3377bdbc7cf1a21415a86207ea416fe92c74
Oladipupo Gomez <Oladipupogomez5@gmail.com>
2023-08-07 18:03:31 -0700

    function that creates an array of chars, and initializes it with a specific char.

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -6,6 +6,7 @@ int **alloc_grid(int width, int height);
 char *str_concat(char *s1, char *s2);
 char *_strdup(char *str);
 int _putchar(char c);
+char *create_array(unsigned int size, char c);
 
 
 #endif
