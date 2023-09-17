8dedaf8cbe775ee9084f99455c97078bb2db8859
Oladipupo Gomez <oladipupogomez5@gmail.com>
2023-09-16 20:59:57 -0700

    task 0 betty issues fixed

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,12 +1,15 @@
 #include <stdarg.h>
 #include "variadic_functions.h"
+
 /**
-*sum_them_all - sums or adds up all parameters
-*@n: number of variables to be summed up
-*@...: variable numebers
-*if n == 0
-*Return 0
-*/
+ * sum_them_all - Returns the sum of all its paramters.
+ * @n: The number of paramters passed to the function.
+ * @...: A variable number of paramters to calculate the sum of.
+ *
+ * Return: If n == 0 - 0.
+ *         Otherwise - the sum of all parameters.
+ */
+
 int sum_them_all(const unsigned int n, ...)
 {
 va_list ap;
